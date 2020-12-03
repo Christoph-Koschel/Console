3b90c7a9a00e15107893fa333bda53f994797eaf
Sleepy-Reacon <63058519+Sleepy-Reacon@users.noreply.github.com>
2020-12-03 17:58:07 +0100

    Console

---
+++ controller.js
@@ -0,0 +1,5 @@
+let WIN;
+
+exports.init = (WIN) => {
+    WIN = arguments[0];
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+const {autoUpdater} = require("electron-updater");
+const {app, BrowserWindow, ipcMain} = require("electron");
+const { init } = require("./controller.js");
+
+app.on("ready", () => {
+
+    let WIN = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    WIN.loadFile("index.html").then(() => {
+        init(WIN);
+    });
+
+    /*
+    * ===================================
+    * == App Updater
+    * ===================================
+    */
+    //region
+
+    WIN.once("ready-to-show", () => {
+        autoUpdater.checkForUpdatesAndNotify();
+    });
+
+    autoUpdater.on('update-available', () => {
+        WIN.webContents.send('update_available');
+    });
+    autoUpdater.on('update-downloaded', () => {
+        WIN.webContents.send('update_downloaded');
+    });
+
+    ipcMain.on("appVersion", (event) => {
+        event.sender.send("appVersion", app.getVersion());
+    });
+
+    ipcMain.on('restart_app', () => {
+        autoUpdater.quitAndInstall();
+    });
+
+
+    //endregion
+
+});
+
+
